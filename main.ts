26b7f2f544f5875516576a6e49a0c08835851329
Luboš Jeřábek <jerabina@users.noreply.github.com>
2020-08-13 18:09:07 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,34 +8,34 @@ basic.forever(function () {
     serial.writeValue("utrasonic", ultrasonic)
     if (ultrasonic < 20) {
         music.playTone(523, music.beat(BeatFraction.Eighth))
-        robotbit.MotorRunDual(
-        robotbit.Motors.M1A,
+        robotAtom.MotorRunDual(
+        robotAtom.Motors.M1A,
         speedBack,
-        robotbit.Motors.M2B,
+        robotAtom.Motors.M2B,
         speedBack
         )
         basic.pause(1000)
         if (Math.randomBoolean()) {
-            robotbit.MotorRunDual(
-            robotbit.Motors.M1A,
+            robotAtom.MotorRunDual(
+            robotAtom.Motors.M1A,
             0,
-            robotbit.Motors.M2B,
+            robotAtom.Motors.M2B,
             speed
             )
         } else {
-            robotbit.MotorRunDual(
-            robotbit.Motors.M1A,
+            robotAtom.MotorRunDual(
+            robotAtom.Motors.M1A,
             speed,
-            robotbit.Motors.M2B,
+            robotAtom.Motors.M2B,
             0
             )
         }
         basic.pause(500)
     } else {
-        robotbit.MotorRunDual(
-        robotbit.Motors.M1A,
+        robotAtom.MotorRunDual(
+        robotAtom.Motors.M1A,
         speed,
-        robotbit.Motors.M2B,
+        robotAtom.Motors.M2B,
         speed
         )
     }
