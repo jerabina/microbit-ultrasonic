1bf61bd6f2f20b5e8134a6d27edf8e993dca0700
Luboš Jeřábek <jerabina@users.noreply.github.com>
2020-09-27 21:30:47 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,42 +1,34 @@
-serial.redirectToUSB()
+function led2 (num: number) {
+    if (num == 1) {
+        range.showColor(neopixel.colors(NeoPixelColors.Green))
+    } else if (num == 2) {
+        range.showColor(neopixel.colors(NeoPixelColors.Red))
+    }
+}
+let range: neopixel.Strip = null
 makerbit.connectUltrasonicDistanceSensor(DigitalPin.P15, DigitalPin.P14)
+range = robotAtom.rgb().range(0, 4)
 let ultrasonic = 50
 let speed = 150
 let speedBack = -150
+led2(1)
 basic.forever(function () {
     ultrasonic = makerbit.getUltrasonicDistance(DistanceUnit.CM)
-    serial.writeValue("utrasonic", ultrasonic)
     if (ultrasonic < 20) {
         music.playTone(523, music.beat(BeatFraction.Eighth))
-        robotAtom.MotorRunDual(
-        robotAtom.Motors.M1A,
-        speedBack,
-        robotAtom.Motors.M2B,
-        speedBack
-        )
+        led2(2)
+        robotAtom.Servo(robotAtom.Servos.S1, 180)
+        robotAtom.MotorRunDual(robotAtom.Motors.M1A, speedBack, robotAtom.Motors.M2B, speedBack)
         basic.pause(1000)
         if (Math.randomBoolean()) {
-            robotAtom.MotorRunDual(
-            robotAtom.Motors.M1A,
-            0,
-            robotAtom.Motors.M2B,
-            speed
-            )
+            robotAtom.MotorRunDual(robotAtom.Motors.M1A, 0, robotAtom.Motors.M2B, speed)
         } else {
-            robotAtom.MotorRunDual(
-            robotAtom.Motors.M1A,
-            speed,
-            robotAtom.Motors.M2B,
-            0
-            )
+            robotAtom.MotorRunDual(robotAtom.Motors.M1A, speed, robotAtom.Motors.M2B, 0)
         }
         basic.pause(500)
     } else {
-        robotAtom.MotorRunDual(
-        robotAtom.Motors.M1A,
-        speed,
-        robotAtom.Motors.M2B,
-        speed
-        )
+        led2(1)
+        robotAtom.Servo(robotAtom.Servos.S1, 0)
+        robotAtom.MotorRunDual(robotAtom.Motors.M1A, speed, robotAtom.Motors.M2B, speed)
     }
 })
