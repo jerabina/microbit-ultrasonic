29f79908b34fa1007d41e7db7ec3fbd29872cd4b
Luboš Jeřábek <jerabina@users.noreply.github.com>
2020-09-27 23:12:38 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,13 @@
 function showLed (num: number) {
     if (num == 1) {
-        range2.showColor(neopixel.colors(NeoPixelColors.Green))
+        range.showColor(neopixel.colors(NeoPixelColors.Green))
     } else if (num == 2) {
-        range2.showColor(neopixel.colors(NeoPixelColors.Red))
+        range.showColor(neopixel.colors(NeoPixelColors.Red))
     }
 }
-let range2: neopixel.Strip = null
+let range: neopixel.Strip = null
 makerbit.connectUltrasonicDistanceSensor(DigitalPin.P15, DigitalPin.P14)
-range2 = robotAtom.rgb().range(0, 4)
+range = robotAtom.rgb().range(0, 4)
 let ultrasonic = 50
 let speed = 150
 let speedBack = -150
